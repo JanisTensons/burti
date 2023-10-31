2561ee60856a9890565381e020a9c7a7cf87eb0e
Janis Tensons <jtensons@gmail.com>
2023-10-31 18:12:43 +0200

    Added new styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import ProductForm from "../../components/ProductForm";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center">
-      <div className="bg-blue-500 text-white py-4 w-full text-center">
-        <h1 className="text-4xl font-semibold">Product Selection</h1>
+    <div className="bg-white min-h-screen flex flex-col items-center">
+      <div className="text-blue-950 py-4 w-full text-center">
+        <h1 className="text-4xl font-semibold mt-10">Product Selection</h1>
       </div>
       <div className="flex justify-center mt-10">
         <ProductForm />
+      </div>
+      <div className="bg-white text-blue-950 text-sm py-2 w-full text-center fixed bottom-0">
+        <p>Made by Janis Tensons | 2023</p>
       </div>
     </div>
   );
